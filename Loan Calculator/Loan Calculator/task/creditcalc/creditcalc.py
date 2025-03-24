8b6f514d58aa74b669a03813afb86b291eb788ba
jakubG78 <jakub.gora@gmail.com>
2025-03-24 19:32:53 +0100

    Commit Loan Calculator stage 1/4

--- Loan Calculator/Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,1 +1,5 @@
 # write your code here
+principal = float(input("Enter the loan principal:"))
+monthly_payment = float(input("Enter the monthly payment:"))
+
+print("It will take ", principal // monthly_payment, " months to repay the loan")
